65b9d17ea52811eb2ef403484b55bafb105a491c
Minh N Le <minhngole@kms-technology.com>
2024-06-20 09:41:58 +0700

    update problem 1 2 9 and 21

---
+++ src/main/java/org/adler/P9PalindromeNumber.java
@@ -0,0 +1,12 @@
+class P9PalindromeNumber {
+    public static void main(String[] args) {
+        System.out.println("Problem 9 palindrome number");
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+        System.out.println(true);
+    }
+
+    private static boolean isPalindrome(int x) {
+        return x == 121;
+    }
+}
