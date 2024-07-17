746c82edcd022e5ce94e5a629d7a56e566bdab11
Minh N Le <minhngole@kms-technology.com>
2024-07-17 16:30:03 +0700

    update structure

--- TypeScript/dist/P9PalindromeNumber.js
+++ TypeScript/dist/P9PalindromeNumber.js
@@ -1,15 +1,17 @@
+"use strict";
 console.log("9. Palindrome Number");
 console.log("Case 1: ", isPalindrome(121));
 console.log("Case 2: ", isPalindrome(-121));
 console.log("Case 3: ", isPalindrome(10));
-var newArr = [1, "2"];
+const newArr = [1, "2"];
 function isPalindrome(x) {
     if (x < 0) {
         return false;
     }
-    var str = x.toString();
-    var revertStr = str.split("").reverse().join("");
+    let str = x.toString();
+    let revertStr = str.split("").reverse().join("");
     if (str == revertStr)
         return true;
     return false;
 }
+//# sourceMappingURL=P9PalindromeNumber.js.map
